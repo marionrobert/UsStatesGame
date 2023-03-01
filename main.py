2cec902bd95eb3ec9ac1aec9f15200f8df831df7
Marion <mrobert148@gmail.com>
2023-03-01 16:47:40 +0100

    check if the user finds a state and fint out the relevant data

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 # print(all_states)
 
+answer_state = screen.textinput(title="Guess the State", prompt="Give another state's name.").capitalize()
 
+if answer_state in all_states:
+    guessed_state = data[data.state == answer_state]
+    x_position = int(guessed_state.x)
+    y_position = int(guessed_state.x)
 
 
 screen.exitonclick()
