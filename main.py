e2a7c78451430a8c9902600374688216f82e07bb
Marion <mrobert148@gmail.com>
2023-03-01 16:53:30 +0100

    write the name of the state guessed on the map at the right position

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import turtle
 import pandas
 
+FONT = FONT = ("Arial", 10, "normal")
+
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
@@ -17,6 +19,13 @@ if answer_state in all_states:
     guessed_state = data[data.state == answer_state]
     x_position = int(guessed_state.x)
     y_position = int(guessed_state.x)
+    new_turtle = turtle.Turtle()
+    new_turtle.hideturtle()
+    new_turtle.penup()
+    new_turtle.goto(x_position, y_position)
+    new_turtle.color("black")
+    new_turtle.write(f"{answer_state}", align="center", font=FONT)
+    
 
 
 screen.exitonclick()
