60ceb73979f461f5ec9713d12e6c4710a65cbcaa
Marion <mrobert148@gmail.com>
2023-03-01 16:46:48 +0100

    create list of all states from csv file with pandas

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+# print(all_states)
+
+
+
+
+screen.exitonclick()
